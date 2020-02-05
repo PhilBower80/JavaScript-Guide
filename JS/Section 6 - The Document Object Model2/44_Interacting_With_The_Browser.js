fe3359c0f64ae273f4ec87af71b20506be8bd188
Philip Bower | Netmatters Ltd <philip.bower@netmatters.local>
2020-02-05 11:05:49 +0000

    page names updated to reflect the course content chapters

---
+++ JS/Section 6 - The Document Object Model2/44_Interacting_With_The_Browser.js
@@ -0,0 +1,13 @@
+const para = document.querySelector('p');
+const paras = document.querySelectorAll('p');
+
+console.log(paras);
+
+// get an element by ID
+const title = document.getElementById('');
+
+// get an element by Class Name
+const title2 = document.getElementsByClassName('.random-content');
+
+// get an element by ID
+const title3 = document.getElementsByTagName('');
