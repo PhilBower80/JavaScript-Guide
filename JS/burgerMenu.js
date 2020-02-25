a1cd474d1857f9ea0612160299e76f864db19a5d
Philip Bower | Netmatters Ltd <philip.bower@netmatters.local>
2020-02-25 15:32:21 +0000

    cookie styles added + side bar functionality added

--- JS/burgerMenu.js
+++ JS/burgerMenu.js
@@ -1,10 +1,20 @@
-const burgerMenu = document.querySelector('#burger-container');
+// SIMPLE CHANGE VISIBILITY
 
-burgerMenu.addEventListener('click', function() {
-	const sidebar = document.querySelector('.nav-sidebar');
-	if (window.getComputedStyle(sidebar).visibility === 'hidden') {
-		sidebar.style.visibility = 'visible';
-	} else {
-		sidebar.style.visibility = 'hidden';
-	}
+// const burgerMenu = document.querySelector('#burger-container');
+
+// burgerMenu.addEventListener('click', e => {
+// 	burgerMenu.classList.toggle('open');
+// 	const sidebar = document.querySelector('.nav-sidebar');
+// 	if (window.getComputedStyle(sidebar).visibility === 'hidden') {
+// 		sidebar.style.visibility = 'visible';
+// 	} else {
+// 		sidebar.style.visibility = 'hidden';
+// 	}
+// });
+
+const burgerMenu = document.querySelector('#burger-container');
+const sidebar = document.querySelector('.nav-sidebar');
+
+burgerMenu.addEventListener('click', e => {
+	sidebar.classList.toggle('active');
 });
