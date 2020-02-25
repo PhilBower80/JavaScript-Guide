6c16b3fbbe4ef097d194bfe3e07afac6c4fbfded
Philip Bower | Netmatters Ltd <philip.bower@netmatters.local>
2020-02-25 17:01:59 +0000

    cookie notification fixed. Bootstrap D-Flex doesnt work with my current JS.

--- JS/burgerMenu.js
+++ JS/burgerMenu.js
@@ -13,10 +13,11 @@
 // });
 
 const burgerMenu = document.querySelector('#burger-container');
-const sidebar = document.querySelector('.nav-sidebar');
-const mainContent = document.querySelector('body');
 
 burgerMenu.addEventListener('click', e => {
+	const sidebar = document.querySelector('.nav-sidebar');
+	burgerMenu.classList.toggle('open');
+	// const mainContent = document.querySelector('body');
 	sidebar.classList.toggle('active');
-	mainContent.classList.toggle('active');
+	// mainContent.classList.toggle('active');
 });
--- JS/cookie.js
+++ JS/cookie.js
@@ -1,18 +1,16 @@
 const cookie = document.querySelector('.cookie-wrapper');
 const closeCookie = document.querySelector('.cookie-close');
-let cookieCheck = localStorage.getItem('cookie');
+const cookieCheck = localStorage.getItem('cookie');
 
 closeCookie.addEventListener('click', () => {
 	localStorage.setItem('cookie', 'true');
 	cookie.style.display = 'none';
-	// console.log(typeof cookieCheck);
 });
 
 if (cookieCheck === 'true') {
 	console.log('cookie = true');
-	cookie.classList.toggle('open');
 	cookie.style.display = 'none';
 } else {
 	console.log('no cookie found');
-	cookie.style.display = 'block';
+	cookie.style.display = 'block'; //used !important to override bootstrap
 }
